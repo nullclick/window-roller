05ba79692a623c56fbbd12962916bed967342f94
Andy Brown <andyb@formulatoast.com>
2015-11-08 19:32:40 -0500

    start creating the overlay form

--- windowroller.dpr
+++ windowroller.dpr
@@ -2,13 +2,15 @@ program windowroller;
 
 uses
   Vcl.Forms,
-  Unit1 in 'source\Unit1.pas' {Form1};
+  Unit1 in 'source\Unit1.pas' {Form1},
+  CigarForm in 'source\CigarForm.pas' {Form2};
 
 {$R *.res}
 
 begin
   Application.Initialize;
-  Application.MainFormOnTaskbar := True;
+  Application.ShowMainForm := true;
+  Application.Title := 'Window Cigar Roller';
   Application.CreateForm(TForm1, Form1);
   Application.Run;
 end.
